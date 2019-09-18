60bda4f37ed50c0e1bb026a9e49c7d829139eb96
chensi0109 <1297109045@qq.com>
2019-09-18 19:01:07 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -2,15 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from './views/Home.vue'
 import Home from './views/home/'
-
+import Login from './views/login/index'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      component: Login
     }
     // {
     //   path: '/about',
