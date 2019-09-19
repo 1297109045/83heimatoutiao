0ac6766266bed517dcd36c62c201c6602247815c
chensi0109 <1297109045@qq.com>
2019-09-19 17:27:08 +0800

    左侧导航完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+// import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$axios = axios
 new Vue({
   router,
   render: h => h(App)
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>我是主页</div>
+<div class="layout-aside">
+  <div class="title">
+    <img src="../../assets/img/logo_admin.png" alt="">
+  </div>
+  <el-menu style="width:201px"
+      background-color="#353b4e"
+      text-color="#adafb5"
+      active-text-color="#ffd04b"
+  >
+    <el-menu-item index="4">
+      <i class="el-icon-s-home"></i>
+      <span slot="title">首页</span>
+    </el-menu-item>
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-document"></i>
+      <span>内容管理</span>
+      </template>
+         <el-menu-item>发布文章</el-menu-item>
+         <el-menu-item>内容列表</el-menu-item>
+         <el-menu-item>评论列表</el-menu-item>
+         <el-menu-item>素材管理</el-menu-item>
+    </el-submenu>
+     <el-submenu index="2">
+      <template slot="title">
+        <i class='el-icon-s-custom'></i>
+      <span>粉丝管理</span>
+      </template>
+         <el-menu-item>发布文章</el-menu-item>
+         <el-menu-item>内容列表</el-menu-item>
+         <el-menu-item>评论列表</el-menu-item>
+         <el-menu-item>素材管理</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="3">
+      <i class="el-icon-setting"></i>
+      <span>账户信息</span>
+    </el-menu-item>
+  </el-menu>
+
+</div>
 </template>
 
 <script>
@@ -8,6 +47,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.title{
+  height: 45px;
+  background-color:#353b4e;
+  width:201px
+}
 </style>
