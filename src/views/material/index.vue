8079b06e3e890cd60a02ac36a780d713abf335fc
chensi0109 <1297109045@qq.com>
2019-09-23 22:37:45 +0800

    全部素材前期完成

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,15 +5,80 @@
             素材管理
         </template>
       </bread-crumb>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="全部素材" name="all">
+        <div class="img-list">
+            <el-card class="img-item" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt="">
+            <div class="operate">
+                <i class="el-icon-star-on"></i>
+                <i class="el-icon-delete-solid"></i>
+            </div>
+
+        </el-card>
+        </div>
+
+    </el-tab-pane>
+    <el-tab-pane label="收藏素材" name="collect">收藏 </el-tab-pane>
+
+  </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all',
+      list: []
+    }
+  },
+  methods: {
+    getMaterial () {
+      this.$axios({
+        url: 'user/images',
+        params: { collect: false }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.img-list{
+    display:flex;
+    flex-wrap:wrap;
+    justify-content: center;
+    .img-item{
+    width: 180px;
+    height: 180px;
+    border-radius:6px;
+    margin: 30px;
+    position: relative;
+    img{
+        width: 100%;
+        height:100%;
+    }
+    .operate{
+        background-color: #f4f5f6;
+        position:absolute;
+        left:0;
+        bottom:0;
+        width:100%;
+        display:flex;
+        justify-content:space-around;
+        align-items: center;
+        i{
+            font-size: 18px
+        }
+    }
 
+    }
+
+}
 </style>
