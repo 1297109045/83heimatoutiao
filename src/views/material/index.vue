4c9505529d91e2e51370e493a51faa62fd5118da
chensi0109 <1297109045@qq.com>
2019-09-24 19:35:19 +0800

    收藏全部分页完成

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,7 +5,7 @@
             素材管理
         </template>
       </bread-crumb>
-      <el-tabs v-model="activeName" @tab-click=" getMaterial">
+      <el-tabs v-model="activeName" @tab-click=" changeType ">
     <el-tab-pane label="全部素材" name="all">
         <div class="img-list">
             <el-card class="img-item" v-for="item in list" :key="item.id">
@@ -17,7 +17,10 @@
 
         </el-card>
         </div>
+      <el-row type="flex" justify="center" style="margin:20px">
+      <el-pagination @current-change="changePage" :page-size="page.pageSize" :current-page="page.currentPage" background layout="prev, pager, next" :total="page.total"></el-pagination>
 
+      </el-row>
     </el-tab-pane>
     <el-tab-pane  label="收藏素材" name="collect">
          <div class="img-list">
@@ -25,6 +28,10 @@
             <img :src="item.url" alt="">
         </el-card>
         </div>
+        <el-row type="flex" justify="center" style="margin:20px">
+      <el-pagination @current-change="changePage" :page-size="page.pageSize" :current-page="page.currentPage" background layout="prev, pager, next" :total="page.total"></el-pagination>
+
+      </el-row>
     </el-tab-pane>
 
   </el-tabs>
@@ -36,17 +43,32 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        total: 0, // 总页数
+        pageSize: 10, // 一页多少条
+        currentPage: 1// 默认第一页
+
+      }
     }
   },
   methods: {
-    getMaterial (collect) {
+    changePage (newPage) {
+      this.page.currentPage = newPage// 更新最新页码给currentpage
+      this.getMaterial()
+    },
+    changeType () {
+      this.page.currentPage = 1
+      this.getMaterial()
+    },
+    getMaterial () {
       // this.activeName === 'collect'找收藏否则找全部
       this.$axios({
         url: 'user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: { collect: this.activeName === 'collect', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
 
