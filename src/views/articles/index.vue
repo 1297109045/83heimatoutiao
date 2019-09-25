081754aaa79a645a64732d592007099397d79052
chensi0109 <1297109045@qq.com>
2019-09-25 19:52:28 +0800

    分页请求

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -6,7 +6,7 @@
     </bread-crumb>
     <el-form style="margin-left:50px">
       <el-form-item label="文章状态:">
-          <!-- v-model来源于 el-radio属性-->
+        <!-- v-model来源于 el-radio属性-->
         <el-radio-group @change="changeCondition" v-model="formData.status">
           <el-radio :label="5">全部</el-radio>
           <el-radio :label="0">草稿</el-radio>
@@ -17,12 +17,17 @@
       </el-form-item>
       <el-form-item label="频道列表:">
         <el-select @change="changeCondition" v-model="formData.channel_id" placeholder="请选择">
-          <el-option  v-for="item in channels" :key="item.id" :value="item.id" :label="item.name">{{item.name}}</el-option>
+          <el-option
+            v-for="item in channels"
+            :key="item.id"
+            :value="item.id"
+            :label="item.name"
+          >{{item.name}}</el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="事件选择:">
         <el-date-picker
-        @change="changeCondition"
+          @change="changeCondition"
           v-model="formData.date"
           type="daterange"
           value-format="yyyy-MM-dd"
@@ -32,22 +37,35 @@
       </el-form-item>
     </el-form>
     <!-- 主体内容 -->
-    <div class="total">共找到555555条符合条件的内容</div>
+    <div class="total">共找到{{page.total}}条符合条件的内容</div>
     <div v-for="(item,index) in list" :key="index" class="article-item">
-        <!-- 布局 -->
-        <div class="left">
-            <img :src="item.cover.images.length?item.cover.images[0]: defaultImg" alt="">
-            <div class="info">
-                <span class="title">{{item.title}}</span>
-                <el-tag :type="item.status | statusType" class="status">{{item.status | satausText}}</el-tag>
-                <span class="data">{{item.pubdate}}</span>
-            </div>
+      <!-- 布局 -->
+      <div class="left">
+        <img :src="item.cover.images.length?item.cover.images[0]: defaultImg" alt />
+        <div class="info">
+          <span class="title">{{item.title}}</span>
+          <el-tag :type="item.status | statusType" class="status">{{item.status | satausText}}</el-tag>
+          <span class="data">{{item.pubdate}}</span>
         </div>
-        <div class="right">
-            <span><i class="el-icon-edit"></i>修改</span>
-            <span><i class="el-icon-delete"></i>删除</span>
-        </div>
+      </div>
+      <div class="right">
+        <span>
+          <i class="el-icon-edit"></i>修改
+        </span>
+        <span>
+          <i class="el-icon-delete"></i>删除
+        </span>
+      </div>
     </div>
+    <el-row type="flex" justify="center" style="margin:10px">
+      <el-pagination
+        background
+        @current-change="changePage"
+        layout="prev, pager, next"
+        :current-page="page.currentpage"
+        :total="page.total"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -62,20 +80,36 @@ export default {
       },
       list: [],
       defaultImg: require('../../assets/img/a.jpg'),
-      channels: []
+      channels: [],
+      page: {
+        totla: 0,
+        currentPage: 1,
+        pageSize: 10
+      }
     }
   },
   methods: {
-    changeCondition () {
-      // 只改变是formdata已经是最新的值
-    //   组装请求参数
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.queryArticles()
+    },
+    queryArticles () {
       let params = {
         status: this.formData.status === 5 ? null : this.formData.status, // 状态为5
         channel_id: this.formData.channel_id,
         begin_pubdate: this.formData.date.length ? this.formData.date[0] : null, // 开始时间
-        end_pubdate: this.formData.date.length > 1 ? this.formData.date[1] : null// 结束时间
+        end_pubdate:
+          this.formData.date.length > 1 ? this.formData.date[1] : null, // 结束时间
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
       }
       this.getArticles(params)
+    },
+    changeCondition () {
+      this.page.currentpage = 1
+      // 只改变是formdata已经是最新的值
+      //   组装请求参数
+      this.queryArticles()
     },
     //   获取频道列表
     getChannels () {
@@ -91,7 +125,8 @@ export default {
         url: '/articles',
         params
       }).then(result => {
-        this.list = result.data.results// 获取文章列表
+        this.list = result.data.results // 获取文章列表
+        this.page.total = result.data.total_count
       })
     }
   },
@@ -134,47 +169,47 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.total{
-    border-bottom: 1px dashed #ccc;
-    height: 50px;
-   line-height: 50px;
+.total {
+  border-bottom: 1px dashed #ccc;
+  height: 50px;
+  line-height: 50px;
 }
-.article-item{
-    display:flex;
-    justify-content: space-between;
-    padding:20px 10px;
-    border-bottom:1px solid #f2f3f5;
-    .left{
-    display:flex;
-    img{
-        width: 180px;
-        height: 100px;
-        border-radius: 4px
+.article-item {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 10px;
+  border-bottom: 1px solid #f2f3f5;
+  .left {
+    display: flex;
+    img {
+      width: 180px;
+      height: 100px;
+      border-radius: 4px;
     }
-    .info{
-    display:flex;
-    height: 100px;
-    flex-direction: column;
-    justify-content: space-around;
-    margin-left:10px;
-    .title{
-        font-size:14px;
-    }
-    .status{
+    .info {
+      display: flex;
+      height: 100px;
+      flex-direction: column;
+      justify-content: space-around;
+      margin-left: 10px;
+      .title {
+        font-size: 14px;
+      }
+      .status {
         width: 60px;
         text-align: convert;
-    }.data{
-            color:#999;
-            font-size:12px
-        }
+      }
+      .data {
+        color: #999;
+        font-size: 12px;
+      }
     }
-
+  }
+  .right {
+    font-size: 12px;
+    span {
+      margin-right: 8px;
     }
-    .right{
-        font-size:12px;
-        span{
-            margin-right:8px;
-        }
-    }
+  }
 }
 </style>
